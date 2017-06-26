94b83c757079c5a2814cc922787565ca6757c5f2
Artem Buria <artem_buria@epam.com>
2017-06-26 17:50:34 +0300

    update for halt

--- halt_all.py
+++ halt_all.py
@@ -5,33 +5,37 @@ import json
 
 
 def json_to_data():
-    machines_info = "C:/Users/artem_buria/.vagrant.d/data/machine-index/index"
+    user_dir = os.environ.get('USERPROFILE')
+    machines_info = user_dir + "/.vagrant.d/data/machine-index/index"
     with open(machines_info) as data:
         jdat = json.loads(data.read())
     return jdat
 
 def machines_act(action):
     json_data = json_to_data()
+    attrib = 'poweroff' if action == 'destroy' else 'running'
     for j in json_data['machines']:
-        if json_data['machines'][j]['state'] == 'running' :
+        state = json_data['machines'][j]['state']
+        if state == attrib :
             machine = json_data['machines'][j]['name']
-            print(machine + " is running right now. It will be {} soon.".format(action), end='\n')
+            print(machine + " is {} right now. It will be {}ed soon.".format(state, action), end='\n')
             os.chdir(json_data['machines'][j]['vagrantfile_path'])
-            os.system("vagrant {}".format(action))
-            print(machine + " is stopped now", end='\n')
+            os.system("vagrant {} {} -f".format(action, machine))
+            print(machine + " is {}ed now".format(action), end='\n')
 
 def main ():
-    if len(sys.argv) == 1 or len(sys.argv) > 2:
+    if len(sys.argv) == 1 or len(sys.argv) > 3:
         print("""
                 WARNING!!!\n
-         You must cpecify one and only one argument, it may be:\n
+         You must specify one and only one argument, it may be:\n
          destroy - to destroy all running machines;\n
-         halt - to halt all running machines; """)
+         halt - to halt all running machines;
+         You can destroy machines only if they are poweroff, and halt them if they are running""")
         sys.exit()
     elif sys.argv[1] == 'destroy':
         machines_act('destroy')
     elif sys.argv[1] == 'halt':
         machines_act('halt')
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
